af0e79f85512132f5769c93aa6228a463edb8e45
Nebil Yisehak <99560401+Nebil1@users.noreply.github.com>
2022-09-22 22:34:07 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # I got you!!
-curl -sL -X PUT -d user_id=98 -H 'Origin: You got me!' 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
