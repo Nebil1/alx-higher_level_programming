dfd94f7fe05259df67916a98bffcfaaa1beddde4
Nebil Yisehak <99560401+Nebil1@users.noreply.github.com>
2022-10-18 22:45:01 +0300

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], (error) => {
-  error && console.log(error);
-});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-request(process.argv[2], function (err, response) {
-  if (err) { console.error(err); }
-  console.log('code:', response && response.statusCode);
-});
