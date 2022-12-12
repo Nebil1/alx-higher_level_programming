5525986c5dd13ec2611c17a6dd7190c9e5cd03ba
Nebil Yisehak <99560401+Nebil1@users.noreply.github.com>
2022-12-12 15:44:09 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+request(process.argv[2], function (err, response) {
+  if (err) { console.error(err); }
+  console.log('code:', response && response.statusCode);
 });
