1cabf863cd10632388a666342faf6af0f9f75b4f
Nebil Yisehak <99560401+Nebil1@users.noreply.github.com>
2022-12-12 15:13:53 +0300

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a given URL with a header and displays the response
+curl -sL -H 'X-School-User-Id: 98' "$1"
